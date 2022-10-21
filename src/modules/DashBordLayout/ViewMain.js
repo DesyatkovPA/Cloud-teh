4e3929d0a4119cf8820ca0eeb084891980b03e62
alex1213e20017250@gmail.com <alex1213e20017250@gmail.com>
2022-10-21 15:23:19 +0300

    3 laba

---
+++ src/modules/DashBordLayout/ViewMain.js
@@ -0,0 +1,14 @@
+import { Container } from "@mui/material";
+
+
+const { default: styled } = require("@emotion/styled");
+
+//const Container = styled.div
+
+export function DashBoardLayout(){
+    return{
+        
+    }
+
+
+}
